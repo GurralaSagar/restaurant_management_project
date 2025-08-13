21f9af5dfebc997d689f439991e21332b3b0bc53
GurralaSagar <156121837+GurralaSagar@users.noreply.github.com>
2025-08-13 14:35:15 +0530

    26619

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("",views.home_view,name='home'),
     path('restaurant/',views.home_view,name='restaurant'),
     path('contact/',views.contact_view,name='contact'),
+    path('about/',views.about_view, name='about'),
 ]
