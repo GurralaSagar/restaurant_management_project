230988b6b29eef17f4a594b4f59d233f84fb93b8
GurralaSagar <156121837+GurralaSagar@users.noreply.github.com>
2025-08-18 11:30:05 +0530

    34430

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('about/',views.about_view, name='about'),
     path('menu/', MenuListView.as_view(), name='menu-list'),
     path("reservations/", views.reservations_view, name="reservations"),
+    path("feedback/", views.feedback_view, name="feedback"),
 ]
