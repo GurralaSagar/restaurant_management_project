01f46adc7d136fc6e4d7703830dc8646d226e903
GurralaSagar <156121837+GurralaSagar@users.noreply.github.com>
2025-08-24 09:43:08 +0530

    37719

--- home/models.py
+++ home/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
 # Create your models here.
-
-class MenuItem(models.Model):
+class Menu(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    items = models.ManyToManyField(menu) # One order can have multiple menu items
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer_name}"
