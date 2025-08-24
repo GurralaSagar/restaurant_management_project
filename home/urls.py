5c8505cfa48f1dab354ec1399a9decdcfe34d4c7
GurralaSagar <156121837+GurralaSagar@users.noreply.github.com>
2025-08-24 09:27:00 +0530

    37720

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-
 from .views import *
 
 urlpatterns = [
-    path("",home_view,name='home'),
-    path('restaurant/',home_view,name='restaurant'),
-    path('contact/',contact_view,name='contact'),
-    path('about/',about_view, name='about'),
+    path("",views.home_view,name='home'),
+    path('restaurant/',views.home_view,name='restaurant'),
+    path('contact/',views.contact_view,name='contact'),
+    path('about/',views.about_view, name='about'),
     path('menu/', MenuListView.as_view(), name='menu-list'),
-    path("reservations/", reservations_view, name="reservations"),
-    #path("feedback/",views.feedback_view, name="feedback"),
+    path("reservations/", views.reservations_view, name="reservations"),
+    path("feedback/", views.feedback_view, name="feedback"),
+    path('api/menu/',menu_view,name='menu'),
 ]
