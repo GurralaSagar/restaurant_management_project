9f50b9b73ecc358ecc26910a83e6bc9a37640c25
GurralaSagar <156121837+GurralaSagar@users.noreply.github.com>
2025-08-27 12:24:03 +0530

    38432

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Menu, Order
-
+from .models import ContactSubmission
 # Register your models here.
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
@@ -12,3 +12,5 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'customer_name', 'created_at')
     search_fields = ('customer_name',)
     filter_horizontal = ('items',) # Better UI for ManyToManyField
+
+admin.site.register(ContactSubmission)
